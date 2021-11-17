dbe543841c60469ff065eeae7501baea07d4821f
Keith <dreamihad@gmail.com>
2021-11-17 18:42:32 -0500

    api key tests

--- pytests/test_lab.py
+++ pytests/test_lab.py
@@ -7,12 +7,18 @@ class TestCtaAPI:
     def api_key(self, pytestconfig):
         return pytestconfig.getoption("api_key")
 
-    def test_print_name(self,api_key):
-        query_params = {'key':api_key,'max':1,'mapid':40360, 'outputType':'JSON'}
-        r = requests.get(self.base_url,params=query_params)
-        text = r.json()
-        print(text)
-        assert r.status_code == 200 
+    def test_api_key_is_valid(self,api_key):
+        # We can only validate for length     
+        assert len(api_key) == 32, 'Invalid API key. '
+    def basic_arrival_request(self,api_key,mapid):
+        query_params = {'key':api_key,'max':1,'mapid':mapid, 'outputType':'JSON'}
+        arrival_resp = requests.get(self.base_url,params=query_params)
+        return arrival_resp
 
-    def test_print_name_2(self,pytestconfig):
-        print(f"test_print_name_2(api_key): {pytestconfig.getoption('api_key')}")
+    def test_arrival_status_code(self, api_key):
+        arrival_resp = self.basic_arrival_request(api_key,40360)
+        assert arrival_resp.status_code == 200 
+    def test_arrival_to_Kimble(self,api_key):
+        arrival_resp = self.basic_arrival_request(api_key,40360)
+        text = arrival_resp.json()
+        print(text)
