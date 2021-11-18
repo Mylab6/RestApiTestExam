178432086fee2bce0b855c297b6010c38962232d
Keith <dreamihad@gmail.com>
2021-11-17 22:18:04 -0500

    GUI updates

--- basic_gui.py
+++ basic_gui.py
@@ -1,17 +1,21 @@
 import PySimpleGUI as sg
 import pytest
 
-TextSize = (60, 1)
+TextSize = (100, 1)
+sg.theme('DarkGrey')
+
 layout = [
     [sg.Text("Welcome to my project", size=TextSize)], [sg.Text(
         "For this excerise I decided to test the Chicago Transit Authority API", size=TextSize)],
-    [sg.Text("Like most useful APIs this requires an API key, this can be retrived easily from CTA's website", size=TextSize)],
+    [sg.Text("Like most APIs this requires an API key, this can be retrived easily from CTA's website", size=TextSize)],
     # add link if time
     # https://www.transitchicago.com/developers/
-    [sg.Text("https://www.transitchicago.com/developers/")],
+    [sg.Text("https://www.transitchicago.com/developers/",size=TextSize )],
     [sg.Text("API Key", size=TextSize)],     # Part 2 - The Layout
-    [sg.Input()],
-    [sg.Button('Run Test')],
+    [sg.Input(key="api_key")],
+    [sg.Input('40360',key="mapid")],
+
+    [sg.Button('Run Test', size=TextSize )],
 
 
     # [sg.Output(size=(80, 20))]
@@ -29,11 +33,12 @@ window = sg.Window('CTA Tests Runner', layout)      # Part 3 - Window Defintion
 
 class MyPlugin:
     def pytest_sessionfinish(self):
+        return 
         print("*** test run reporting finishing")
         print(dir(self.pytest_sessionfinish))
 
     def pytest_runtest_logreport(self, report):
-        print(dir(report))
+        print(report.outcome)
 
 
 # Display and interact with the Window using an Event Loop
@@ -42,7 +47,7 @@ while True:
     # See if user wants to quit or window was closed
     if event == "Run Test":
         retcode = pytest.main(
-            ["-s",  "-x", "pytests", "--api_key", "aaa"], plugins=[MyPlugin()])
+            ["-s",  "-x", "pytests", "--api_key", values['api_key'],"--mapid",values['mapid'] ], plugins=[MyPlugin()])
         print(vars(retcode))
         window.refresh()
 
--- pytests/test_lab.py
+++ pytests/test_lab.py
@@ -1,26 +1,44 @@
 import pytest
 import requests
 
+
 class TestCtaAPI:
     base_url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx"
+
     @pytest.fixture()
     def api_key(self, pytestconfig):
         return pytestconfig.getoption("api_key")
 
-    def test_api_key_is_valid(self,api_key):
-        # We can only validate for length     
+    @pytest.fixture()
+    def mapid(self, pytestconfig):
+        return pytestconfig.getoption("mapid")
+
+    def test_api_key_is_valid(self, api_key):
+        # We can only validate for length
         assert len(api_key) == 32, 'Invalid API key. '
-    def basic_arrival_request(self,api_key,mapid, numberOfTrains=1 ):
-        query_params = {'key':api_key,'max':numberOfTrains,'mapid':mapid, 'outputType':'JSON'}
-        arrival_resp = requests.get(self.base_url,params=query_params)
+
+    def basic_arrival_request(self, api_key, mapid, numberOfTrains=1):
+        query_params = {'key': api_key, 'max': numberOfTrains,
+                        'mapid': mapid, 'outputType': 'JSON'}
+        arrival_resp = requests.get(self.base_url, params=query_params)
         return arrival_resp
 
     def test_arrival_status_code(self, api_key):
-        arrival_resp = self.basic_arrival_request(api_key,40360)
-        assert arrival_resp.status_code == 200 
-    def test_station_name_is_Southport(self,api_key):
-        arrival_resp = self.basic_arrival_request(api_key,40360, 3)
+        arrival_resp = self.basic_arrival_request(api_key, 40360)
+        assert arrival_resp.status_code == 200
+
+    def test_station_name_is_Southport(self, api_key):
+        arrival_resp = self.basic_arrival_request(api_key, 40360, 3)
         trainResp = arrival_resp.json()
-        for trainArrival in trainResp['ctatt']['eta']:        
+        for trainArrival in trainResp['ctatt']['eta']:
             assert trainArrival['staNm'] == "Southport"
-        print(trainResp)
+
+    def test_dynamtic_mapid(self, api_key, mapid):
+        arrival_resp = self.basic_arrival_request(api_key, mapid, 3)
+        trainResp = arrival_resp.json()
+        assert arrival_resp.status_code == 200
+        try:
+            for trainArrival in trainResp['ctatt']['eta']:
+                assert trainArrival['staNm'] is not None
+        except:
+            assert False, str(mapid) + ' is not a valid mapid or has no comming arrivals '
