73f526bb266c641d347ad54792680dff2834c1f4
Keith <dreamihad@gmail.com>
2021-11-17 20:59:21 -0500

    Gui Tweaks

--- basic_gui.py
+++ basic_gui.py
@@ -1,24 +1,28 @@
-import PySimpleGUI as sg                  
+import PySimpleGUI as sg
 import pytest
-layout = [ 
-     [sg.Text("Welcome to my project , for this excerise I decided to test the Chicago Transit Authority API \n Like most useful APIs this requires an API key, this can be retrived easily from CTA's website")] ,    
-   # add link if time 
-   # https://www.transitchicago.com/developers/
+
+TextSize = (60, 1)
+layout = [
+    [sg.Text("Welcome to my project", size=TextSize)], [sg.Text(
+        "For this excerise I decided to test the Chicago Transit Authority API", size=TextSize)],
+    [sg.Text("Like most useful APIs this requires an API key, this can be retrived easily from CTA's website", size=TextSize)],
+    # add link if time
+    # https://www.transitchicago.com/developers/
     [sg.Text("https://www.transitchicago.com/developers/")],
-     [sg.Text("API Key")],     # Part 2 - The Layout
-            [sg.Input()],
-            [sg.Button('Run Test')] , 
-            
-            
-           # [sg.Output(size=(80, 20))]
-            ]
-            
+    [sg.Text("API Key", size=TextSize)],     # Part 2 - The Layout
+    [sg.Input()],
+    [sg.Button('Run Test')],
+
+
+    # [sg.Output(size=(80, 20))]
+]
+
 
 # Create the window
 window = sg.Window('CTA Tests Runner', layout)      # Part 3 - Window Defintion
-                                                
+
 # Display and interact with the Window
-#event, values = window.read()                   # Part 4 - Event loop or Window.read call
+# event, values = window.read()                   # Part 4 - Event loop or Window.read call
 
 # Do something with the information gathered
 
@@ -29,7 +33,7 @@ class MyPlugin:
         print(dir(self.pytest_sessionfinish))
 
     def pytest_runtest_logreport(self, report):
-            print(dir(report))
+        print(dir(report))
 
 
 # Display and interact with the Window using an Event Loop
@@ -37,17 +41,12 @@ while True:
     event, values = window.read()
     # See if user wants to quit or window was closed
     if event == "Run Test":
-      retcode = pytest.main(["-s",  "-x","pytests","--api_key","aaa"], plugins=[MyPlugin()])
-      print( vars(retcode)) 
-      window.refresh()
-    
+        retcode = pytest.main(
+            ["-s",  "-x", "pytests", "--api_key", "aaa"], plugins=[MyPlugin()])
+        print(vars(retcode))
+        window.refresh()
+
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
-    # Output a message to the window
-    #window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
 
-
-#print('Hello', values[0], "! Thanks for trying PySimpleGUI")
-
-# Finish up by removing from the screen
-window.close()   
+window.close()
