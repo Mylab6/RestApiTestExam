f636e43d9d3f393d338dac30e317a4777d97ff61
Keith <dreamihad@gmail.com>
2021-11-17 23:21:36 -0500

    select text

--- basic_gui.py
+++ basic_gui.py
@@ -3,6 +3,11 @@ import pytest
 
 TextSize = (100, 1)
 sg.theme('Black')
+init_map_id = 40360
+init_station_name = 'Southport'
+sample_train_stops = {"Lake": 41660, 'Western': 41480, 'Southport': 40360}
+train_buttons = list(map(lambda train_stop_name: sg.Button(
+    train_stop_name), sample_train_stops.keys()))
 
 layout = [
     [sg.Text("Welcome to my project", size=TextSize)], [sg.Text(
@@ -10,10 +15,13 @@ layout = [
     [sg.Text("Like most APIs this requires an API key, this can be retrived easily from CTA's website", size=TextSize)],
     # add link if time
     # https://www.transitchicago.com/developers/
-    [sg.Text("https://www.transitchicago.com/developers/",size=TextSize )],
-    [sg.Text("API Key", size=TextSize),sg.Input(key="api_key")],
-    [sg.Text("Mapid", size=TextSize ),sg.Input('40360',key="mapid")],
-
+    [sg.Text("https://www.transitchicago.com/developers/", size=TextSize)],
+    [sg.Text("Mapid"), sg.Input(init_map_id, key="mapid")],
+    [sg.Text("Expected Station Name"), sg.Input(
+        init_station_name, key="expected_station_name")],
+    [sg.Text("Select Station",size=TextSize)],
+    train_buttons,
+    [sg.Text("API Key"), sg.Input(key="api_key")],
     [sg.Button('Run Test')],
 
 
@@ -32,7 +40,7 @@ window = sg.Window('CTA Tests Runner', layout)      # Part 3 - Window Defintion
 
 class GetTestResults:
     def pytest_sessionfinish(self):
-        return 
+        return
         print("*** test run reporting finishing")
         print(dir(self.pytest_sessionfinish))
 
@@ -44,9 +52,15 @@ class GetTestResults:
 while True:
     event, values = window.read()
     # See if user wants to quit or window was closed
+
+    if event in sample_train_stops:
+        window.Element("mapid").update(sample_train_stops[event])
+        window.Element("expected_station_name").update(event)
+        #init_map_id = sample_train_stops[event]
+        window.refresh()
     if event == "Run Test":
         retcode = pytest.main(
-            ["-s",  "-x", "pytests", "--api_key", values['api_key'],"--mapid",values['mapid'] ], plugins=[GetTestResults()])
+            ["-s",  "pytests", "--api_key", values['api_key'], "--mapid", values['mapid'], "--expected_station_name",values['expected_station_name']  ], plugins=[GetTestResults()])
         print(vars(retcode))
         window.refresh()
 
--- pytests/test_lab.py
+++ pytests/test_lab.py
@@ -17,34 +17,38 @@ class TestCtaAPI:
     def expected_station_name(self, pytestconfig):
         return pytestconfig.getoption("expected_station_name")
 
-    def get_first_train(self, trainResp):
-        return trainResp['ctatt']['eta'][0]
-    
-    def test_api_key_is_valid(self, api_key):
-        # We can only validate for length
-        assert len(api_key) == 32, 'Invalid API key. '
-
     def basic_arrival_request(self, api_key, mapid, numberOfTrains=1):
         query_params = {'key': api_key, 'max': numberOfTrains,
                         'mapid': mapid, 'outputType': 'JSON'}
         arrival_resp = requests.get(self.base_url, params=query_params)
         return arrival_resp
 
-    def test_arrival_status_code(self, api_key,mapid):
+    def get_first_train(self, trainResp):
+        return trainResp['ctatt']['eta'][0]
+
+    def test_api_key_is_valid(self, api_key):
+        # We can only validate for length
+        assert len(api_key) == 32, 'Invalid API key. '
+
+    def test_arrival_status_code(self, api_key, mapid):
         arrival_resp = self.basic_arrival_request(api_key, mapid)
+        # CTA's API will always return a 200, see the next assertion.
         assert arrival_resp.status_code == 200
+        parsed_resp = arrival_resp.json()
+        assert parsed_resp['ctatt']['errCd'] == "0", parsed_resp['ctatt']['errNm']
 
     def test_40360_station_name_is_Southport(self, api_key):
         arrival_resp = self.basic_arrival_request(api_key, 40360)
         trainResp = arrival_resp.json()
         first_train = self.get_first_train(trainResp)
-        assert first_train['staNm'] == "Southport"
+        assert first_train['staNm'] == 'Southport'
 
-    def test_dynamtic_mapid(self, api_key, mapid,expected_station_name):
+    def test_dynamtic_mapid(self, api_key, mapid, expected_station_name):
         arrival_resp = self.basic_arrival_request(api_key, mapid)
-        trainResp = arrival_resp.json()      
+        trainResp = arrival_resp.json()
         try:
             first_train = self.get_first_train(trainResp)
             assert first_train['staNm'] == expected_station_name
         except:
-            assert False, str(mapid) + ' is not a valid mapid or has no comming arrivals '
+            assert False, str(
+                mapid) + ' is not a valid mapid or has no comming arrivals '
